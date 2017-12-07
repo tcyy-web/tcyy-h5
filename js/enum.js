8bd176b6abd1f4000e7b8329d6eba8cd479d9293
GuoJie <1335161515@qq.com>
2017-12-07 15:32:40 +0800

    项目

---
+++ js/enum.js
@@ -0,0 +1,26 @@
+/*枚举*/
+;
+(function(w){
+	w.enums = {
+		sex: {
+			1:'男' ,2: '女'
+		},
+		category: {
+			1: 'iconfont icon-icon',
+			2: 'iconfont icon-kouqiangbaojian',
+			3: 'iconfont icon-kouqiangmeirong',
+			4: 'iconfont icon-kouqiangneike',
+			5: 'iconfont icon-kouqiangwaike',
+			6: 'iconfont icon-kouqiangxiufu',
+			7: 'iconfont icon-kouqiangzhengji',
+			8: 'iconfont icon-kouqiangzhongzhi',
+		},
+		getEnumLabel: function(type, key) {
+			var _this = this;
+			if (_this[type]) {
+				return _this[type][key] || '';
+			}
+			return '';
+		}
+	}
+})(window);
