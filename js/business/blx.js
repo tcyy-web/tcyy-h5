c884a58f12ea53129a9e5527e9de2ca85e8fb72c
gj <1335161515@qq.com>
2018-03-07 17:12:18 +0800

    病例秀修改

---
+++ js/business/blx.js
@@ -0,0 +1,73 @@
+;
+(function(w){
+  w.blxFn = {
+    // 获取详情
+    detailFn: function(caseid, callback) {
+      request.loginAjax('cases/getDetails', {
+        data: {
+          id: id
+        },
+        showMsg: false
+      }, function(data, success) {
+        if (success) {
+          callback(data);
+        }
+      });
+    },
+    // 点赞
+    dzFn: function(caseid, callback) {
+      request.loginAjax('cases/thing', {
+        data: {
+          case_id: caseid
+        }
+      }, function(data, success) {
+        if (success) {
+          callback();
+        }
+      });
+    },
+    // 关注
+    gzFn: function(uid, callback) {
+      request.loginAjax('cases/follow', {
+        data: {
+          touid: uid
+        }
+      }, function(data, success) {
+        if (success) {
+          callback(data);
+        }
+      });
+    },
+    // 删除病例秀
+    delFn: function(caseid, callback) {
+      mui.confirm('确定删除？', '删除病例秀', null, function(obj){
+        if (obj.index === 1){
+          request.loginAjax('cases/delDraft', {
+            data: {
+              case_id: caseid
+            }
+          }, function(data, success) {
+            if (success) {
+              callback();
+            }
+          });
+        }
+      }, 'div');
+    },
+    // 列表获取点赞的人
+    dzUsersFn: function(caseid, callback) {
+      request.loginAjax('cases/getThingList', {
+        data: {
+          case_id: caseid,
+          page: 1,
+          limit: 5
+        },
+        showMsg: false
+      }, function(data, success) {
+        if (success && data && data.data.length > 0) {
+          callback(data.data);
+        }
+      });
+    }
+  }
+})(window);
