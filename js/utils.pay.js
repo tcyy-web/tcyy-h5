02734da152512adddd1fc7362372fb1b4db0b0c5
gj <1335161515@qq.com>
2019-10-22 17:51:30 +0800

    课程支付

--- js/utils.pay.js
+++ js/utils.pay.js
@@ -1,6 +1,24 @@
 ;
 (function(w){
   w.payApi = {
-    
+    // 获取支付参数
+    getWxPayParams: function(payurl, callback) {
+      var xhr=new XMLHttpRequest();
+      xhr.onreadystatechange=function(){
+        switch(xhr.readyState){
+          case 4:
+            if(xhr.status==200){
+              callback(xhr.responseText);
+            }else{
+              plus.nativeUI.alert("获取支付参数失败");
+            }
+            break;
+          default:
+            break;
+        }
+      }
+      xhr.open('get', payurl);
+      xhr.send();
+    }
   }
 })(window);
